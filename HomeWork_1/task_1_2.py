aecb463b9b5390e51525bd59b85b454cea4ff651
Denis <wwworkandget@yandex.com>
2022-12-27 18:35:16 +0300

    Added  homework workshop 1 code

---
+++ HomeWork_1/task_1_2.py
@@ -0,0 +1,15 @@
+# Задача 2.
+# Пользователь вводит время в секундах. Переведите время в часы, минуты и секунды и выведите в формате чч:мм:сс.
+# Используйте форматирование строк.
+
+time_sec = int(input('Введите число: '))
+hours = time_sec//3600
+hours_res = (hours) if hours > 10 else ('0' + str(hours))
+minutes = (time_sec % 3600)//60
+minutes_res = (minutes) if minutes > 10 else ('0' + str(minutes))
+seconds = (time_sec % 3600) % 60
+seconds_res = (seconds) if seconds > 10 else ('0' + str(seconds))
+if hours > 24:
+    print('Количество полученных часов превышает количество часов в сутка, скоректируйте ввод.')
+else:
+    print(f'Московское время: {hours_res}:{minutes_res}:{seconds_res}')
